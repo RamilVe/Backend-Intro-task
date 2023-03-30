e84202df6985fd1251628192935e7c66a182b0a1
Ramil <ramilev@code.edu.az>
2023-03-30 09:35:53 +0400

    project added

---
+++ Backend Intro/Backend Intro/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend_Intro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return Content("Id: " + id);
+        }
+    }
+}
